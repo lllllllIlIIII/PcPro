06a73498c3669d8664b2d823cf0cf34afde7de82
samed baritci <samed.baritci@condorcet.be>
2026-06-07 15:11:36 +0200

    commit : change allinclude

--- admin/src/php/utils/all_include.php
+++ admin/src/php/utils/all_include.php
@@ -1,8 +1,22 @@
 <?php
 
-require_once 'admin/src/php/db/db_pg_connect.php';
+$isAdmin = str_contains($_SERVER['REQUEST_URI'], 'admin');
 
-require_once 'admin/src/php/classes/autoloader.class.php';
+if ($isAdmin) {
+    $pathDb = 'admin/src/php/db/db_pg_connect.php';
+    $pathAutoloader = 'admin/src/php/classes/Autoloader.class.php';
+} else {
+    $pathDb = 'admin/src/php/db/db_pg_connect.php';
+    $pathAutoloader = 'admin/src/php/classes/Autoloader.class.php';
+}
 
-Autoloader::register();
+if (file_exists($pathDb) && file_exists($pathAutoloader)) {
+    include $pathDb;
+    include $pathAutoloader;
+
+    Autoloader::register();
+    $pdo = Connexion::getInstance($dsn, $user, $pass);
+} else {
+    die("Impossible de charger les fichiers de configuration.");
+}
 ?>
