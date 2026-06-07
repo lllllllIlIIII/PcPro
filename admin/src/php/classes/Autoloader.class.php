300eaf7eccd8d76ab84bda518cf1b2663a7ce657
samed baritci <samed.baritci@condorcet.be>
2026-06-07 15:10:08 +0200

    commit : change autoloader

--- admin/src/php/classes/Autoloader.class.php
+++ admin/src/php/classes/Autoloader.class.php
@@ -1,15 +1,14 @@
 <?php
 class Autoloader {
-
     static function register() {
         spl_autoload_register(array(__CLASS__, 'autoload'));
     }
 
     static function autoload($class) {
-        $path = 'admin/src/php/classes/' . $class . '.class.php';
+        $filePath = 'admin/src/php/classes/' . $class . '.class.php';
 
-        if (file_exists($path)) {
-            require_once $path;
+        if (file_exists($filePath)) {
+            require_once $filePath;
         }
     }
 }
